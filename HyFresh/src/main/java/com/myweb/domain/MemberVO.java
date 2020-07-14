16d685a1a61ac5567c101721b00da8f9775c16f5
EZEN <EZEN@DESKTOP-5TFCUFR>
2020-07-14 12:29:19 +0900

    create MemberVO

---
+++ HyFresh/src/main/java/com/myweb/domain/MemberVO.java
@@ -0,0 +1,16 @@
+package com.myweb.domain;
+
+import java.util.Date;
+
+public class MemberVO {
+	private int mno;
+	private String email;
+	private String pwd;
+	private String name;
+	private int addrno;
+	private String tel;
+	private Date birth;
+	private int grade;
+	private Date regd8;
+	private int point;
+}
